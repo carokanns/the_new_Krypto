d36bb4ba88f1065fdf5b695944eb3f7469aef023
carokanns <jp@fendrich.se>
2023-04-07 12:03:54 +0200

    implement progress bar.

--- my_crypto.py
+++ my_crypto.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[31]:
 
 
 #%% 
 # Komplett omtag av new_crypto.py
-# HACK: Påbörjad dummy - fixa resten
-# TODO: Inkludera inflationsgraf med grafer ovan
 
 # TODO: Lägg till valet av my own crypto och hantera dem precis som ovan
 # TODO: Kopier new_skapa_modeller.ipynb till my_testa_modeller.ipynb
-# TODO: Skapa 1 (en) modell för prediction av valfri krypto
-# TODO: Testa olika modelltyper i my_skapa_modeller.ipynb
+# TODO: Skapa 1 (en) modell för prediction av valfri krypto.
+# TODO: Testa olika modelltyper och bestäm vilken som skall användas i my_skapa_modeller.ipynb
 
 # TODO: Flera sidor?
 # TODO: Slutligen: Byt namn till new_crypto.py igen innan publicering
 #%%
-
-
-# In[32]:
-
 
 import pandas as pd
 import numpy as np
@@ -30,67 +23,13 @@ from binance.client import Client
 import datetime
 
 
-# In[33]:
-
-
-@st.cache_data
-def get_data():
-
-    api_key = '2jxiCQ8OIWmU4PZH4xfwKEY9KYerDkSWzwNCqoaMzj41eJgWBsSqA3VYqkt2wmdX'
-    api_secret = 'YY1Qj1t0JZrE4tQdaBBxT8iwl2tbFalWp1FHjyZ9selBb6OnQ0Oj8aVdiXO7YLMz'
-
-    client = Client(api_key, api_secret)
-
-    # Hämta handelspar
-    symbols = client.get_all_tickers()
-    symbols = [symbol for symbol in symbols if symbol['symbol'].endswith('USDT')]
-
-    # Sätt upp en tom lista för att lagra close-priser
-    close_prices = {}
-
-    # Ange den tidsram du vill ha för historiska data
-    interval = Client.KLINE_INTERVAL_1DAY
-    start_time = "2 month ago UTC"
-
-    # Hämta close-priser för alla kryptovalutor
-    dates = None
-     #add a counter to print a message every 100 symbols
-    counter = 0
-    for symbol in symbols:   
-        try:
-            klines = client.get_historical_klines(symbol['symbol'], interval, start_time)
-            if dates is None:
-                # Extrahera och konvertera tidsstämplar till datum
-                dates = [datetime.datetime.fromtimestamp(int(kline[0]) / 1000).strftime('%Y-%m-%d') for kline in klines]
-            close_prices[symbol['symbol']] = [float(kline[4]) for kline in klines]
-            # every 100 symbols print a message
-            if counter % 100 == 0:
-                print(f"Hämtade {counter} symboler")
-        except Exception as e:
-            print(f"Kunde inte hämta data för {symbol['symbol']}: {e}")
-        counter += 1
-    # Konvertera close_prices-dikten till en pandas DataFrame
-    df = pd.DataFrame.from_dict(close_prices, orient='index').transpose()  
-    # Lägg till datum som index för DataFrame
-    df.index = pd.to_datetime(dates)                                   # type: ignore
-    print(df.head())
-    return df
-df=get_data()
-
-
 # ## Inflation
-
-# In[34]:
-
 
 def add_horizon_columns(inflation, horizons):
     for horizon in horizons:
         inflation['US_inflation_'+str(horizon)] = inflation['US_inflation'].rolling(horizon, 1).mean()
         inflation['SE_inflation_'+str(horizon)] = inflation['SE_inflation'].rolling(horizon, 1).mean()
     return inflation
-
-
-# In[35]:
 
 
 def initiate_data(inflation, df_dates, lang_dict, value_name):
@@ -107,9 +46,6 @@ def initiate_data(inflation, df_dates, lang_dict, value_name):
     inflation[value_name] = inflation[value_name].astype(float)
     inflation[value_name].interpolate(method='linear', inplace=True)
     return inflation
-
-
-# In[36]:
 
 
 #%%
@@ -149,24 +85,78 @@ def get_inflation_data():
     return inflations
 
 
-
-# In[37]:
-
-
 st.title('Performance av Kryptovalutor')
+MAX_MONTHS = 24
 
+@st.cache_data
+def get_data():
+
+    api_key = '2jxiCQ8OIWmU4PZH4xfwKEY9KYerDkSWzwNCqoaMzj41eJgWBsSqA3VYqkt2wmdX'
+    api_secret = 'YY1Qj1t0JZrE4tQdaBBxT8iwl2tbFalWp1FHjyZ9selBb6OnQ0Oj8aVdiXO7YLMz'
+
+    client = Client(api_key, api_secret)
+
+    # Hämta handelspar
+    symbols = client.get_all_tickers()
+    symbols = [
+        symbol for symbol in symbols if symbol['symbol'].endswith('USDT')]
+
+    # Sätt upp en tom lista för att lagra close-priser
+    close_prices = {}
+
+    # Ange den tidsram du vill ha för historiska data
+    interval = Client.KLINE_INTERVAL_1DAY
+    start_time = f"{MAX_MONTHS} month ago UTC"
+
+    # Hämta close-priser för alla kryptovalutor
+    dates = None
+
+    progress_bar = st.progress(0)  # Create a progress bar
+
+    for idx, symbol in enumerate(symbols):
+        try:
+            klines = client.get_historical_klines(
+                symbol['symbol'], interval, start_time)
+            if dates is None:
+                # Extrahera och konvertera tidsstämplar till datum
+                dates = [datetime.datetime.fromtimestamp(
+                    int(kline[0]) / 1000).strftime('%Y-%m-%d') for kline in klines]
+            close_prices[symbol['symbol']] = [
+                float(kline[4]) for kline in klines]
+        except Exception as e:
+            print(f"Kunde inte hämta data för {symbol['symbol']}: {e}")
+
+        # Update the progress bar
+        progress_text = f"This will take several minutes. Symbol number {idx+1} of {len(symbols)}: . . . . . . . . {symbol['symbol']}"
+        progress_bar.progress((idx + 1) / len(symbols), progress_text)
+        if idx+1 == len(symbols):
+            st.snow()
+    # Konvertera close_prices-dikten till en pandas DataFrame
+    df = pd.DataFrame.from_dict(close_prices, orient='index').transpose()
+    # Lägg till datum som index för DataFrame
+    # type: ignore
+    df.index = pd.to_datetime(dates)  # type: ignore
+    print(df.head())
+
+    return df
+
+df = get_data()
 
 months = int(st.number_input(
-    'Please enter the number of previous months', min_value=1, max_value=48, value=12))
+    'Please enter the number of previous months', min_value=1, max_value=MAX_MONTHS, value=12))
 n_examine = int(st.number_input(
     'Please enter the number of tickers to examine', min_value=1, max_value=25, value=10))
 
 #%%
 
 
+@st.cache_data
 def get_returns(df, months):
     target_date = df.index[-1] - DateOffset(months=months)
-
+    st.write('Target date is: ', target_date)
+    st.write('Most recent date is: ', df.index[-1])
+    st.write('oldest date is: ', df.index[0])
+    st.write('Number of rows in dataframe: ', len(df))
     # Find the index that is closest to the target_date and does not exceed it
     closest_index = df.index[df.index.get_loc(target_date, method='pad')]
     st.write('Closest date to target date is: ', closest_index)
@@ -188,13 +178,10 @@ st.table(winners)
 st.table(losers)
 
 
-# In[40]:
-
-
-bestPick = st.selectbox('Pick one for graph', winners.index, index=0)
+bestPick = st.selectbox('Pick one of the best for graph', winners.index, index=0)
 st.line_chart(df[bestPick]) # type: ignore
 
-worstPick = st.selectbox('Pick one for graph', losers.index, index=0)
+worstPick = st.selectbox('Pick one of the worst for graph', losers.index, index=0)
 st.line_chart(df[worstPick]) # type: ignore
 
 # make a line graph of the inflations
